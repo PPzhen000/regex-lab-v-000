805fed420f3bcddfc88e5545387a723b5527260a
Ariel Zhen <zhenpingping@live.com>
2018-05-24 22:34:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   if (text.match(/^un/) != nil && text.match(/ing$/) != nil)
-    return text
+    return text.to_a
   else
     return false
   end
