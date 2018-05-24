f2eae80ca77f9f79c0c46dbdd2da7b0f00dea3d6
Ariel Zhen <zhenpingping@live.com>
2018-05-24 22:27:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,19 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if (text.match(/^un/) != nil && text.match(/ing$/) != nil)
+    return true
+  else
+    return false
+  end
 end
 
 def words_five_letters_long(text)
-
+  if (text.scan(/\D/)).length == 5)
+    return true
+  else
+    return false
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
