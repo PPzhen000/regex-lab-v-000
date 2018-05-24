0df76c17c20829b55f0c7c5dd87fe2943e3e29a5
Ariel Zhen <zhenpingping@live.com>
2018-05-24 22:31:36 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   # else
   #   return false
   # end
-  text.match(/^un/) != nil && text.match(/ing$/) != nil
+  text.match(/^un/) && text.match(/ing$/)
 end
 
 def words_five_letters_long(text)
