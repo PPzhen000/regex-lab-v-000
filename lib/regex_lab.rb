1301d00fca54b2ab1236b24f183855a45fa21e4c
Ariel Zhen <zhenpingping@live.com>
2018-05-25 06:47:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
    if word[0].match(/[aeiouAEIOU]/)
-     return true 
-   else 
-     return false 
-   end 
+     return true
+   else
+     return false
+   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
