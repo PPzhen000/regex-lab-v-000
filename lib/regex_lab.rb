7116e845478e0eefc56425287415a2895d6cefae
Ariel Zhen <zhenpingping@live.com>
2018-05-25 06:53:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,13 +11,21 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match (/^[A-Z].+\W$/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match (/.?\d{3}.?\d{3}.?\d{4}\b/)
+    return true
+  else
+    return false
+  end
 end
